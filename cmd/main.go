eb453f59088b4298121bea00fcf176b9143ee4f8
Edwinfpirajan <efpa1998@hotmail.com>
2023-08-06 07:37:41 -0500

    generated migrations

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/Edwinfpirajan/practice.git/config"
+	"github.com/Edwinfpirajan/practice.git/migrations"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	config.ConnectionDatabase()
+	migrations.GenerateMigration()
 
 	e := echo.New()
 
--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,5 +1,10 @@
 package migrations
 
-func GenerateMigration() {
+import (
+	"github.com/Edwinfpirajan/practice.git/config"
+	"github.com/Edwinfpirajan/practice.git/models"
+)
 
+func GenerateMigration() {
+	config.DB.AutoMigrate(&models.User{}, &models.Team{})
 }
